257eece423432740c17841c676730947aa3c449f
nmm224 <nmm224@lehigh.edu>
2023-10-07 22:10:20 -0400

    initial commit for web using react

---
+++ web/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import "./style.css"
+import * as ReactDOM from "react-dom";
+
+const Hello = () => <h1>Hello from React</h1>;
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
