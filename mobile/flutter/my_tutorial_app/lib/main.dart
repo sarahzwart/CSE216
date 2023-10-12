0f934d686efc7ee5c5f28abc5c10301891e8bb37
Sarah Zwart <scz225@lehigh.edu>
2023-10-12 13:54:17 -0400

    Main.dart dart code added, still need to send likes/messages to backend

--- mobile/flutter/my_tutorial_app/lib/main.dart
+++ mobile/flutter/my_tutorial_app/lib/main.dart
@@ -3,194 +3,176 @@ import 'package:english_words/english_words.dart';
 import 'dart:developer' as developer;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'models/NumberWordPair.dart';
 import './net/webRequests.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Message {
+  String text; //the message
+  bool isLiked; //if it is liked by the user
+  int likes; //number of likes
 
-  // This widget is the root of your application.
+  Message(this.text, this.likes, this.isLiked);
+}
+
+/*
+Stateful Widgets can maintain and update their internal state.
+They are used when you need to create interactive UI elements that change 
+in response to user input or data changes.
+The setState method is used to trigger a rebuild of the widget 
+when the internal state changes.
+*/
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  _MessageAppState createState() => _MessageAppState();
+}
+
+class _MessageAppState extends State<MyApp> {
+  final List<Message> messages = [];
+  bool userIsLoggedIn = true;
+  //URL of Backend
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: HttpReqWords(),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class HttpReqWords extends StatefulWidget {
-  const HttpReqWords({super.key});
-
-  @override
-  State<HttpReqWords> createState() => _HttpReqWordsState();
-}
-
-class _HttpReqWordsState extends State<HttpReqWords> {
-  // final _words = <String>[]; // also try: var _words = <String>['a', 'b', 'c', 'd'];
-  var _words = <String>['a', 'b', 'c', 'd'];
-  final _biggerFont = const TextStyle(fontSize: 18);
-  late Future<List<String>> _future_words;
-  late Future<List<NumberWordPair>> _future_list_numword_pairs;
-
-  @override
-  void initState() {
-    super.initState();
-    _future_words = doSomeLongRunningCalculation();
-    _future_list_numword_pairs = fetchNumberWordPairs();
-  }
-
-  void _retry() {
-    setState(() {
-      _future_list_numword_pairs = fetchNumberWordPairs();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return build_v3(context);
-  }
-
-  Widget build_v3(BuildContext context) {
-    var fb = FutureBuilder<List<NumberWordPair>>(
-      future: _future_list_numword_pairs,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<NumberWordPair>> snapshot) {
-        Widget child;
-
-        if (snapshot.hasData) {
-          // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
-          // create  listview to show one row per array element of json response
-          child = ListView.builder(
-              //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
-              padding: const EdgeInsets.all(16.0),
-              itemCount: snapshot.data!.length,
-              itemBuilder: /*1*/ (context, i) {
-                return Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text(
-                        "row ${i}: num=${snapshot.data![i].num} str=${snapshot.data![i].str}",
-                        // snapshot.data![i].str,
-                        style: _biggerFont,
-                      ),
-                    ),
-                    Divider(height: 1.0),
-                  ],
-                );
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Anonymous Posts',
+            style: TextStyle(
+              fontFamily: 'San Francisco',
+              color: Color.fromARGB(255, 0, 43, 117),
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 179, 187, 255),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return MessageTile(
+                    message: messages[index],
+                    onLike: () async {
+                      final message = messages[index];
+                      // Increment the likes for the message
+                      setState(() {
+                        message.isLiked = !message.isLiked;
+                        if (messages[index].isLiked) {
+                          messages[index].likes++;
+                        } else {
+                          messages[index].likes--;
+                        }
+                      });
+                    }
+                  );
+                },
+              ),
+            ),
+            MessageInput(onMessageAdded: (messageText) {
+              // Add a new message to the list
+              final newMessage = Message(messageText, 0, false);
+              setState(() {
+                messages.add(newMessage);
               });
-        } else if (snapshot.hasError) {
-          // newly added
-          child = Text('${snapshot.error}');
-        } else {
-          // awaiting snapshot data, return simple text widget
-          // child = Text('Calculating answer...');
-          child = const CircularProgressIndicator(); //show a loading spinner.
-        }
-        return child;
-      },
+            }),
+          ],
+        ),
+      ),
     );
-
-    return fb;
   }
-
-  /// we're leaving a lot of "code debris" while we learn with prior versions of the build
-  /// it helps us see iterations that worked, but we should clean this up when we're done
 }
 
-Future<List<String>> doSomeLongRunningCalculation() async {
-  return getWebData();
-  // return getWebData(); // we'll try this next.
+class MessageTile extends StatelessWidget {
+  final Message message;
+  final VoidCallback onLike;
+
+  const MessageTile({required this.message, required this.onLike});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Text(message.text),
+        subtitle: Text('Likes: ${message.likes}'),
+        trailing: LikeButton(
+          isLiked: message.isLiked,
+          onPressed: onLike,
+        ),
+      ),
+    );
+  }
 }
 
-Future<List<String>> simpleLongRunningCalculation() async {
-  await Future.delayed(Duration(seconds: 5)); // wait 5 sec
-  List<String> myList = List.generate(
-      100, (index) => WordPair.random().asPascalCase,
-      growable: true);
-  return myList;
-  // return ['x', 'y', 'z']; // you can hard code the result if you want it more predictable
+//Make a heart that fills in when liked and unfills if unliked, also keep
+//track of the likes from all users
+class LikeButton extends StatelessWidget {
+  final bool isLiked;
+  final VoidCallback onPressed;
+
+  LikeButton({required this.isLiked, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isLiked ? Icons.favorite : Icons.favorite_border,
+        color: Colors.pink,
+      ),
+      onPressed: onPressed,
+    );
+  }
 }
+
+class MessageInput extends StatefulWidget {
+  final ValueChanged<String> onMessageAdded;
+
+  MessageInput({required this.onMessageAdded});
+
+  @override
+  _MessageInputState createState() => _MessageInputState();
+}
+
+class _MessageInputState extends State<MessageInput> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                hintText: 'Enter a message...',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {
+              final messageText = _textController.text;
+              if (messageText.isNotEmpty) {
+                widget.onMessageAdded(messageText);
+                _textController.clear();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 Future<List<String>> getWebData() async {
   developer.log('Making web request...');
