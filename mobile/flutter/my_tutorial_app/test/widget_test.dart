07b88e85553cbf55fbf977734c14b00c2ab0f7a4
Sarah Zwart <scz225@lehigh.edu>
2023-10-15 21:08:41 -0400

    Unit tests

--- mobile/flutter/my_tutorial_app/test/widget_test.dart
+++ mobile/flutter/my_tutorial_app/test/widget_test.dart
@@ -5,26 +5,43 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_tutorial_app/main.dart';
+import 'package:my_tutorial_app/models/Message.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('Like value should be incremented', (){
+    //Create a message with 1 like
+    final Message message = Message(text: 'Test Message', likes: 0, isLiked: false);
+    //Increment the Likes
+    message.likes = message.likes + 1;
+    //Expect that the likes are equal to 1
+    expect(message.likes, 1);
+  });
+  test('Like value should be decremented', () {
+    //Create a message with 1 like
+    final Message message = Message(text: 'Test Message', likes: 1, isLiked: true);
+    //Decrement the like count
+    message.likes = message.likes - 1;
+    //Expect that the likes have been decremented
+    expect(message.likes, 0);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Toggle like status and adjust likes', () {
+    //Create message with 0 likes
+    final Message message = Message(text: 'Test Message', likes: 0, isLiked: false);
+    //Change like status to true
+    message.isLiked = true;
+    message.likes = message.likes + 1;
+    //Expect isLiked has changed to true
+    expect(message.isLiked, true);
+    //Expect likes have been incremented by 1
+    expect(message.likes, 1);
+    //Toggle the like status again, which should decrement likes
+    message.isLiked = false;
+     message.likes = message.likes - 1;
+    //Expect isLiked has changed to false
+    expect(message.isLiked, false);
+    //Expect likes have been decremented
+    expect(message.likes, 0);
   });
 }
