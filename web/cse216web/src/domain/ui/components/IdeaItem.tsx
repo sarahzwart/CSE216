94c1c8a9b4f94413aef308a6edc561c8ed1cd754
Sarah Zwart <scz225@lehigh.edu>
2023-11-03 16:37:47 -0400

    Implementing Idea List

--- web/cse216web/src/domain/ui/components/IdeaItem.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import {Message} from '../../entitites/Message';
-
-class IdeaItem extends Component {
-
-}
-export default IdeaItem;
--- web/cse216web/src/domain/ui/components/IdeaList.tsx
+++ web/cse216web/src/domain/ui/components/IdeaList.tsx
@@ -1,10 +1,14 @@
 import React, { useState , useEffect} from 'react';
 import { Component } from 'react';
 import {Message} from '../../entitites/Message';
+import {Comment} from '../../entitites/Comment';
+import {User} from '../../entitites/User';
 import axios from 'axios';
 
 function IdeaList(){
   const [messages, setMessages] = useState<Message[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -19,6 +23,29 @@ function IdeaList(){
         setIsLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    const url = 'https://team-margaritavillians.dokku.cse.lehigh.edu/comments/'
+    axios.get(url).then((response) => {
+        const commentData: Comment[] = response.data.mData;
+        setComments(commentData);
+      })
+      .catch(error => {
+        console.error('Error when fetching message:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const url = 'https://team-margaritavillians.dokku.cse.lehigh.edu/users/'
+    axios.get(url).then((response) => {
+        const userData: User[] = response.data.mData;
+        setUsers(userData);
+      })
+      .catch(error => {
+        console.error('Error when fetching message:', error);
+      });
+  }, []);
+
   return(
     <div>
       <h1>Idea List</h1>
@@ -27,15 +54,28 @@ function IdeaList(){
       ) : (
         messages.map((message, index) => (
           <div key={index}>
-            <h2>{message.mTitle}</h2>
+            <h2>{displayUsername(message.uId)}</h2>
             <p>{message.mMessage}</p>
             <p>Likes: {message.mLikes}</p>
-            <p>User ID: {message.uId}</p>
+            <h3>Comments:</h3>
+            <ul>
+              {messageComments(message.mId)}
+            </ul>
           </div>
         ))
       )}
     </div>
   );
+  function messageComments(messageId: number) {
+    const commentsForMessage = comments.filter(comment => comment.mId === messageId);
+    return commentsForMessage.map((comment) => (
+      <li key={comment.cId}>{comment.cContent}</li>
+    ));
+  }
+  function displayUsername(userId: number){
+    const user = users.find(user => user.uId === userId);
+    return user ? user.uName : 'Unknown User';
+  }
 }
 
 export default IdeaList;
