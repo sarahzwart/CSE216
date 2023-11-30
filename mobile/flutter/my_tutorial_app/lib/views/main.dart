0f11aa7fd9b47b05af20e6f9f78c89280a13b78a
Kamil Gjondla <kamil@dyn222048.cc.lehigh.edu>
2023-11-30 14:54:09 -0500

    added some flutter camera frameworks

--- mobile/flutter/my_tutorial_app/lib/views/main.dart
+++ mobile/flutter/my_tutorial_app/lib/views/main.dart
@@ -1,3 +1,5 @@
+import 'package:camera/camera.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../net/webRequests.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,9 +13,12 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 //deleting duplicate files: find . -type f -name '* [0-9]' -exec rm {} +, find . -type f -name '* [0-9].*' -exec rm {} +
 const String backendURL = 'http://team-margaritavillians.dokku.cse.lehigh.edu';
+final Future<SharedPreferences> prefs = SharedPreferences.getInstance();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
   ByteData data = await rootBundle.load('lib/assets/ca/lets-encrypt-r3.pem');
   SecurityContext.defaultContext
       .setTrustedCertificatesBytes(data.buffer.asUint8List());
@@ -315,6 +320,88 @@ class CommentInputState extends State<MessageInput> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class TakePictureScreen extends StatefulWidget{
+  const TakePictureScreen({
+    super.key,
+    required this.camera
+  });
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen>{
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.done){
+            return CameraPreview(_controller);
+          }
+          else{
+            return const Center(child: CircularProgressIndicator());
+          }
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            if(!mounted) return;
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      body: Image.file(File(imagePath)),
     );
   }
 }
